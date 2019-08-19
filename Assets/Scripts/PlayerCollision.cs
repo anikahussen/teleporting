4715b0d8d6462a87082e82fdd3feae3eda237392
Anika Hussen <anika.hussen@gmail.com>
2019-08-19 14:53:56 -0400

    first commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+
+    void OnCollisionEnter(Collision collisioninfo)
+    {
+        if (collisioninfo.gameObject.tag == "border")
+        {
+            Debug.Log("HIT");
+        }
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float sidewaysForce = 1000f;
+
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+
+
+}
